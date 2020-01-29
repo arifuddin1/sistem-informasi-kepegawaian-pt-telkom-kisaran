fa6efb68e6f1864b0182344c4b07abeb2091a32f
arifuddin1 <60189586+arifuddin1@users.noreply.github.com>
2020-01-29 21:32:48 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,33 @@
+<div class="container">
+    <div class="row">
+        <br>
+		<br>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Kepegawaian Pada PT. Telkom Kisaran</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/telkom.png" width="350" height="250"></img></p>
+                          <p class="label-title" align="center"><strong>PT TELKOM INDONESIA, Tbk</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Kepegawaian Pada PT. Telkom Kisaran.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data Pegawai Telkom<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data Pegawai.<br>
+                            Untuk mengelola Data Pegawai, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12"><center><font face = "nyala">
+            Copyright &COPY; <?= date('Y')?> Jamaluddin, Lucky Oktaviansah, Rizki Ramadhan, Tri Purnomo Aji |
+            <a href="#">Dokumentasi</a>
+            <a href="#">Peta Situs</a>
+            <a href="#">Support</a>
+			<center></font>
+        </div>
+    </div>
+</div>
